524eabc5718e3eddd92a2e717c9815dc2c7e9862
Ivan Nasekin <nasekinid8591@yandex.ru>
2025-11-13 22:22:24 +0300

    feat: add algotithms

---
+++ algorithms/1_contest/d.py
@@ -0,0 +1,18 @@
+# Решето эратосфена
+
+def eratosthenes(n):
+    if n < 2:
+        return 0
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    p = 2
+    while p * p <= n:
+        if is_prime[p]:
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+
+    return sum(is_prime)
+
+print(eratosthenes(int(input())))
